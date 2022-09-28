08edec86366084b65f1eab809d556d73fa04973a
psycodev <92122576+psycodev@users.noreply.github.com>
2022-09-27 22:44:26 -0500

    add files via upload
    esta es la vencida

---
+++ cotalite4/Usuarios/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from . import views
+#from Usuarios.views import userListView
+from django.urls import path
+
+
+urlpatterns = [
+        path('register/', views.register, name='register'),
+        path('listar/', views.listarUsu, name='empleados'),
+        path('editar/<str:id>', views.updateusu, name='updateusu'),
+        path('eliminar/<str:id>', views.deleteUser, name='deleteUser'),
+        path('consultar/',views.consultemp, name='buscar usuarios'),
+        path('listarbyid/<str:id>/',views.listusubyid, name='buscar id'),
+
+        
+    ]
+    
